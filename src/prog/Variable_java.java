cce37d3cb388c3129b2b9b0ef7559a6d89285ae2
mani <maniblaze81@gmail.com>
2023-05-12 19:22:20 +0530

    variable prog

---
+++ src/prog/Variable_java.java
@@ -0,0 +1,16 @@
+package prog;
+
+public class Variable_java {
+	public static void main(String args[])
+	{
+
+		int a=3;
+		int b=4;
+		int rectangle= a*b;
+		System.out.println(rectangle);
+
+
+
+	}
+
+}
